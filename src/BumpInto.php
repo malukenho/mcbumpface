cb2cc55907f76c608f63ce6d063cd27ba601730d
Jefersson Nathan <admin@phpse.net>
2018-06-20 23:50:16 +0200

    Add script to update versions
    Signed-off-by: Jefersson Nathan <admin@phpse.net>

---
+++ src/BumpInto.php
@@ -0,0 +1,161 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Malukenho\McBumpface;
+
+use Composer\Composer;
+use Composer\EventDispatcher\EventSubscriberInterface;
+use Composer\IO\IOInterface;
+use Composer\Json\JsonManipulator;
+use Composer\Package\Link;
+use Composer\Package\Locker;
+use Composer\Package\RootPackageInterface;
+use Composer\Plugin\PluginInterface;
+use Composer\Script\Event;
+use Composer\Script\ScriptEvents;
+use Generator;
+use function array_key_exists;
+use function array_merge;
+use function file_get_contents;
+use function file_put_contents;
+use function iterator_to_array;
+use function sprintf;
+use function str_replace;
+use function strpos;
+
+final class BumpInto implements PluginInterface, EventSubscriberInterface
+{
+    public static function versions(Event $composerEvent) : void
+    {
+        $io               = $composerEvent->getIO();
+        $composer         = $composerEvent->getComposer();
+        $rootPackage      = $composer->getPackage();
+        $composerJsonFile = $composer->getConfig()->getConfigSource()->getName();
+
+        $contents    = file_get_contents($composerJsonFile);
+        $manipulator = new JsonManipulator($contents);
+
+        $lockVersions        = iterator_to_array(self::getInstalledVersions($composer->getLocker(), $rootPackage));
+        $requiredVersions    = self::getRequiredVersion($rootPackage);
+        $requiredDevVersions = self::getRequiredDevVersion($rootPackage);
+
+        self::updateDependencies($io, $manipulator, 'require', $requiredVersions, $lockVersions);
+        self::updateDependencies($io, $manipulator, 'require-dev', $requiredDevVersions, $lockVersions);
+
+        file_put_contents($composerJsonFile, $manipulator->getContents());
+    }
+
+    /**
+     * @param string[] $requiredVersions
+     * @param string[] $lockVersions
+     */
+    private static function updateDependencies(
+        IOInterface $IO,
+        JsonManipulator $manipulator,
+        string $configKey,
+        array $requiredVersions,
+        array $lockVersions
+    ) : void {
+        foreach ($requiredVersions as $package => $version) {
+            if (! array_key_exists($package, $lockVersions)) {
+                continue;
+            }
+            $lockVersion = $lockVersions[$package];
+
+            if (self::isSimilar($version, $lockVersion)) {
+                continue;
+            }
+
+            $manipulator->addLink($configKey, $package, '^' . $lockVersion, false);
+
+            $IO->write(sprintf(
+                'Updating <info>%s</info>%s package from version (<info>%s</info>) to (<info>%s</info>)',
+                $package,
+                $configKey === 'require-dev' ? ' dev' : '',
+                $version,
+                '^' . $lockVersion
+            ));
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function activate(Composer $composer, IOInterface $io)
+    {
+        // nope.
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public static function getSubscribedEvents() : array
+    {
+        return [
+            ScriptEvents::POST_INSTALL_CMD => 'versions',
+            ScriptEvents::POST_UPDATE_CMD => 'versions',
+        ];
+    }
+
+    /**
+     * @return Generator|string[]
+     */
+    private static function getInstalledVersions(Locker $locker, RootPackageInterface $rootPackage) : Generator
+    {
+        $lockData                 = $locker->getLockData();
+        $lockData['packages-dev'] = $lockData['packages-dev'] ?? [];
+        foreach (array_merge($lockData['packages'], $lockData['packages-dev']) as $package) {
+            yield $package['name'] => $package['version'];
+        }
+
+        foreach ($rootPackage->getReplaces() as $replace) {
+            $version = $replace->getPrettyConstraint();
+            if ($version === 'self.version') {
+                $version = $rootPackage->getVersion();
+            }
+            yield $replace->getTarget() => $version;
+        }
+
+        yield $rootPackage->getName() => $rootPackage->getVersion();
+    }
+
+    /**
+     * @return Generator|string[]
+     */
+    private static function getRequiredVersion(RootPackageInterface $rootPackage) : array
+    {
+        return iterator_to_array(self::extractVersions($rootPackage->getRequires()));
+    }
+
+    /**
+     * @return Generator|string[]
+     */
+    private static function getRequiredDevVersion(RootPackageInterface $rootPackage) : array
+    {
+        return iterator_to_array(self::extractVersions($rootPackage->getDevRequires()));
+    }
+
+    /**
+     * @param Link[] $links
+     *
+     * @return Generator|string[]
+     */
+    private static function extractVersions(array $links) : Generator
+    {
+        foreach ($links as $packageName => $required) {
+            // should only consider packages with `/` separator
+            // that means that we ignore "php" or "ext-*"
+            if (strpos($packageName, '/') === false) {
+                continue;
+            }
+
+            yield $packageName => $required->getConstraint()->getPrettyString();
+        }
+    }
+
+    private static function isSimilar(string $version, string $lockVersion) : bool
+    {
+        return str_replace('^', '', $version) === str_replace('^', '', $lockVersion);
+    }
+}
---
+++ test/BumpIntoTest.php
@@ -0,0 +1,157 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MalukenhoTest\McBumpface;
+
+use Composer\Composer;
+use Composer\Config;
+use Composer\IO\IOInterface;
+use Composer\Package\Link;
+use Composer\Package\Locker;
+use Composer\Package\RootPackageInterface;
+use Composer\Script\Event;
+use Composer\Semver\Constraint\ConstraintInterface;
+use Malukenho\McBumpface\BumpInto;
+use org\bovigo\vfs\vfsStream;
+use org\bovigo\vfs\vfsStreamFile;
+use PHPUnit\Framework\TestCase;
+use function file_get_contents;
+use function json_decode;
+use function sprintf;
+use function uniqid;
+
+final class BumpIntoTest extends TestCase
+{
+    /**
+     * @test
+     *
+     * @param string[] $expected expected end structure
+     *
+     * @dataProvider providerVersions
+     */
+    public function updateVersions(string $requiredPackage, string $requiredVersion, string $installedVersion, array $expected) : void
+    {
+        $fileName = uniqid('file', true) . '.json';
+        $fakeDir  = vfsStream::setup('fakeDir');
+
+        $fileStream = new vfsStreamFile($fileName);
+        $fileStream->write(sprintf('{
+            "require": {
+                "%s": "%s"
+            }
+        }', $requiredPackage, $requiredVersion));
+
+        $fakeDir->addChild($fileStream);
+
+        $composerEvent = $this->createMock(Event::class);
+        $IOInterface   = $this->createMock(IOInterface::class);
+        $composer      = $this->createMock(Composer::class);
+        $config        = $this->createMock(Config::class);
+        $configSource  = $this->createMock(Config\ConfigSourceInterface::class);
+        $package       = $this->createMock(RootPackageInterface::class);
+
+        $composerEvent
+            ->expects(self::once())
+            ->method('getIO')
+            ->willReturn($IOInterface);
+
+        $composerEvent
+            ->expects(self::once())
+            ->method('getComposer')
+            ->willReturn($composer);
+
+        $composer
+            ->expects(self::once())
+            ->method('getConfig')
+            ->willReturn($config);
+
+        $composer
+            ->expects(self::once())
+            ->method('getPackage')
+            ->willReturn($package);
+
+        $config
+            ->expects(self::once())
+            ->method('getConfigSource')
+            ->willReturn($configSource);
+
+        $configSource
+            ->expects(self::once())
+            ->method('getName')
+            ->willReturn(vfsStream::url('fakeDir') . '/' . $fileName);
+
+        $locker = $this->createMock(Locker::class);
+        $locker
+            ->expects(self::once())
+            ->method('getLockData')
+            ->willReturn([
+                'packages' => [
+                    [
+                        'name' => $requiredPackage,
+                        'version' => $installedVersion,
+                    ],
+                ],
+             ]);
+
+        $package
+            ->expects(self::once())
+            ->method('getReplaces')
+            ->willReturn([]);
+
+        $composer
+            ->expects(self::once())
+            ->method('getLocker')
+            ->willReturn($locker);
+
+        $link       = $this->createMock(Link::class);
+        $constraint = $this->createMock(ConstraintInterface::class);
+
+        $link
+            ->expects(self::once())
+            ->method('getConstraint')
+            ->willReturn($constraint);
+
+        $constraint
+            ->expects(self::once())
+            ->method('getPrettyString')
+            ->willReturn($requiredVersion);
+
+        $package
+            ->expects(self::once())
+            ->method('getRequires')
+            ->willReturn([$requiredPackage => $link]);
+
+        $package
+            ->expects(self::once())
+            ->method('getDevRequires')
+            ->willReturn([]);
+
+        BumpInto::versions($composerEvent);
+
+        $composerFinalContent = file_get_contents(vfsStream::url('fakeDir') . '/' . $fileName);
+
+        self::assertSame(['require' => $expected], json_decode($composerFinalContent, true));
+    }
+
+    /**
+     * @return string[][]
+     */
+    public function providerVersions() : array
+    {
+        return [
+            [
+                'package' => 'malukenho/docheader',
+                'required_version' => '^1.0',
+                'installed_version' => '1.0.0',
+                'expected' => ['malukenho/docheader' => '^1.0.0'],
+            ],
+            [
+                'package' => 'malukenho/docheader',
+                'required_version' => '^1.3',
+                'installed_version' => '1.9.6',
+                'expected' => ['malukenho/docheader' => '^1.9.6'],
+            ],
+        ];
+    }
+}
